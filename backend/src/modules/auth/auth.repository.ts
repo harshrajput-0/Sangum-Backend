08e12203e494a6c074a46dffc510aabc358f74e4
Harsh Rajput <harshrajput0426@gmail.com>
2026-06-23 12:45:22 +0530

    added more funtion

--- backend/src/modules/auth/auth.repository.ts
+++ backend/src/modules/auth/auth.repository.ts
@@ -12,9 +12,33 @@ export const findByUserId = ( userId: string ) => {
     return Account.findOne({ userId });
 }
 
-// find by id
+// find by accountId
 export const findById = ( accountId: string ) => {
     return Account.findById({ accountId }).select(" +refreshToken ");
 }
 
+// Create Account
+export const createAccount = (data: Partial<IAccount>) => {
+    return Account.create(data);
+}
 
+// Update Refresh Token using hashedToken
+ export const updateRefreshToken = (accountId: string, hashedToken: string) => {
+    return Account.findByIdAndUpdate( accountId, { refreshToken: hashedToken } );
+ };
+
+
+// Clearing refreshToken 
+export const clearRefreshToken = (accountId: string) => {
+    return Account.findByIdAndUpdate( accountId, { $unset: {refreshToken: 1}})
+}
+
+// Set Emaill in Account using accountId  
+export const setEmail = (accountId: string, email: string) => {
+    return Account.findByIdAndUpdate( accountId, { email }, { new: true });
+};
+
+// Check Email in Account  
+export const checkEmailExists = (email: string) => {
+    return Account.exists({ email });
+};
