a119c57280053d69d8011ae1cf62ac223fad4368
Harsh Rajput <harshrajput0426@gmail.com>
2026-06-30 17:22:55 +0530

    starting controllers

--- backend/src/modules/auth/auth.controller..ts
+++ backend/src/modules/auth/auth.controller..ts
@@ -2,6 +2,23 @@ import asyncHandler from "../../utils/asyncHandler";
 import ApiResponse from "../../utils/ApiResponse";
 import ApiError from "../../utils/ApiError";
 
+import * as authService from "./auth.services";
+
 import { Request, Response } from "express";
 import { IAccount } from "./account.model";
 import { IUser } from "../users/user.model";
+import { env } from "../../config/env";
+import { access } from "node:fs";
+
+
+const REFRESH_COOKIE_NAME = "sangum_refresh_token";
+
+const setRefreshCookie = (res: Response, token: string) => {
+    res.cookie(REFRESH_COOKIE_NAME, token, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+};
+
+
