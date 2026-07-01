622f8d2d5e56076eed39ae45c8f856307905a64f
Harsh Rajput <harshrajput0426@gmail.com>
2026-07-01 22:51:08 +0530

    role.middleware and css files added

---
+++ backend/src/middlewares/role.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import ApiError from "../utils/ApiError";
+import { UserRole } from "../modules/users/user.model";
+
+export const requireRole = (...allowedRoles: UserRole[]) => {
+    return (req: Request, _res: Response, next: NextFunction) => {
+        if(!req.user){
+            return next(ApiError.unauthorized());
+        }
+
+        if(!allowedRoles.includes(req.user.role)) {
+            return next(ApiError.forbidden("You don't have permission to do this"));
+        }
+
+        next();
+    };
+};
