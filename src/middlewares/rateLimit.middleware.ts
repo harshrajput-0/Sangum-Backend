a66d434746bf3b76e6bbe939bda942a7d2ec45c8
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-12 12:00:02 +0530

    removed testing condition from rateLimit

--- src/middlewares/rateLimit.middleware.ts
+++ src/middlewares/rateLimit.middleware.ts
@@ -10,7 +10,7 @@ export const authRateLimit = rateLimit({
   max: 10, // 10 attempts per IP per window
   standardHeaders: true,
   legacyHeaders: false,
-  skip: () => env.NODE_ENV === "test", // 👈 don't let tests trip the real limiter
+  // skip: () => env.NODE_ENV === "test", // 👈 Only for testing purpose
   message: {
     success: false,
     statusCode: 429,
