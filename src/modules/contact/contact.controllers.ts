5f9759b89c34ad9507ccd45efde7271e809da654
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-13 11:08:28 +0530

    Before moving from gmail to resend

--- src/modules/contact/contact.controllers.ts
+++ src/modules/contact/contact.controllers.ts
@@ -3,7 +3,7 @@ import ApiResponse from "../../utils/ApiResponse.js";
 import * as contactServices from "./contact.service.js";
 
 export const sendMessage = asyncHandler(async (req, res) => {
-  await contactServices.sendMessage(req.body);
+  contactServices.sendMessage(req.body);    // removed await
 
   res.status(200).json(
     new ApiResponse(200, undefined, "Message sent successfully")
--- src/modules/contact/contact.service.ts
+++ src/modules/contact/contact.service.ts
@@ -1,22 +1,17 @@
 import { sendEmail } from "../../services/email.service.js";
 import type { ContactInput } from "./contact.validation.js";
 
-export const sendMessage = async ({
-  name,
-  email,
-  message,
-}: ContactInput) => {
-  await sendEmail({
+// contact.service.ts
+export const sendMessage = ({ name, email, message }: ContactInput) => {
+  sendEmail({
     to: process.env.EMAIL_USER!,
     subject: `New contact message from ${name}`,
     html: `
       <h2>New Contact Message</h2>
-
       <p><strong>Name:</strong> ${name}</p>
       <p><strong>Email:</strong> ${email}</p>
-
       <h3>Message</h3>
       <p>${message}</p>
     `,
-  });
+  }).catch((err) => console.error("Contact email failed:", err));
 };
